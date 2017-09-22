2e044844b4d4230ca18429566cf06d3c93b3dd60
Amit Kakade <Prerana@Amits-MacBook-Pro.local>
2017-09-22 13:48:18 -0400

    Initial commit

---
+++ resources/views/recipe.blade.php
@@ -0,0 +1,98 @@
+
+
+
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Healthy Gluten Free Snacks.</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div>
+
+        <B>Kale chips</B>
+        <br/>
+        Ingredients <br/>
+        <ul>
+        <li>Bunch of kale</li>
+        <li>Olive oil</li>
+        <li> Salt</li>
+    </ul>         
+       <B>Method</B>
+        <br/>
+        <p>
+        Turn the oven on and set temperature to 350.<br/> 
+        Wash and dry kale leaves.<br/> 
+        Chop them into bite size pieces.<br/> 
+        Sprinkle with olive oil and salt.<br/> 
+        Transfer contents to cookie tray.<br/> 
+        Bake for about 15 mins or till the leaves turn brown on the edges.<br/> 
+        Let it cool and enjoy this tasty and healthy snack.<br/> 
+
+
+
+    </p>
+
+</div>
+</body>
+</html>
